c290944e67ff422ced96a0550893d62da68e732c
JeanPaulCP <140226335+JeanPaulCP@users.noreply.github.com>
2025-07-10 19:09:50 -0500

    Actualizo login

--- LoginUsuario.py
+++ LoginUsuario.py
@@ -8,45 +8,78 @@ def hash_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
 
 def lambda_handler(event, context):
-    # Entrada (json)
-    user_id = event['body']['user_id']
-    password = event['body']['password']
-    tabla_usuarios = os.environ['TABLE_USER']
-    tabla_token = os.environ['TABLE_TOKEN']
-    hashed_password = hash_password(password)
-    # Proceso
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table(tabla_usuarios)
-    response = table.get_item(
-        Key={
-            'user_id': user_id
-        }
-    )
-    if 'Item' not in response:
-        return {
-            'statusCode': 403,
-            'body': 'Usuario no existe'
-        }
-    else:
-        hashed_password_bd = response['Item']['password']
-        if hashed_password == hashed_password_bd:
-            # Genera token
-            token = str(uuid.uuid4())
-            fecha_hora_exp = datetime.now() + timedelta(minutes=60)
-            registro = {
-                'token': token,
-                'expires': fecha_hora_exp.strftime('%Y-%m-%d %H:%M:%S')
+    print(event)
+
+    try:
+        tenant_id = event['body']['tenant_id']
+        username = event['body']['username']
+        password = event['body']['password']
+        hashed_password = hash_password(password)
+
+        # Obtener nombres de tablas desde variables de entorno
+        tabla_usuarios = os.environ['TABLE_NAME_USERS']
+        tabla_token = os.environ['TABLE_NAME_TOKENS']
+
+        # Acceder a DynamoDB
+        dynamodb = boto3.resource('dynamodb')
+        table_users = dynamodb.Table(tabla_usuarios)
+
+        # Buscar usuario con tenant_id y username
+        response = table_users.get_item(
+            Key={
+                'tenant_id': tenant_id,
+                'username': username
             }
-            table = dynamodb.Table(tabla_token)
-            dynamodbResponse = table.put_item(Item=registro)
-        else:
+        )
+
+        if 'Item' not in response:
+            return {
+                'statusCode': 403,
+                'body': 'Usuario no existe en este tenant'
+            }
+
+        print(response)
+
+        user_item = response['Item']
+        hashed_password_db = user_item['password']
+
+        # Validar password
+        if hashed_password != hashed_password_db:
             return {
                 'statusCode': 403,
                 'body': 'Password incorrecto'
             }
 
-    # Salida (json)
-    return {
-        'statusCode': 200,
-        'token': token
-    }
+        # Generar token
+        token = str(uuid.uuid4())
+        fecha_hora_exp = datetime.utcnow() + timedelta(minutes=60)
+
+        table_tokens = dynamodb.Table(tabla_token)
+        table_tokens.put_item(
+            Item={
+                'tenant_id': tenant_id,
+                'token': token,
+                'username': username,
+                'expires': fecha_hora_exp.strftime('%Y-%m-%d %H:%M:%S')
+            }
+        )
+        responseMessage = {
+            'statusCode': 200,
+            'body': {
+                'message': 'Login exitoso',
+                'token': token,
+                'expires': fecha_hora_exp.strftime('%Y-%m-%d %H:%M:%S'),
+                'tenant_id': tenant_id,
+                'username': username
+            }
+        }
+        print(responseMessage)
+
+        return responseMessage
+
+    except Exception as e:
+        print("Exception:", str(e))
+        return {
+            'statusCode': 500,
+            'body': {'error': str(e)}
+        }
